2328a4d569f9c4a5bdc9dd318dd53883a8844d12
Radouane Aouini <radouane.aouini@etu.u-paris.fr>
2024-10-26 19:22:09 +0200

    \#3 ajouts fonctions pour placer les bateau dans plateau

--- bataille_navale/lib/plateau.ml
+++ bataille_navale/lib/plateau.ml
@@ -4,9 +4,31 @@ type bateau = (int * int) list
 type case = | Vide | Bateau of bateau | Touche | Rate
 type plateau = {
   grille : case array array;  
-  ships : bateau list;     
+  mutable ships : bateau list;     
 }
 
 let plateau_vide = Array.init 10  (fun _ -> Array.init 10 (fun _ -> Vide))
 
+let init_plateau () : plateau = 
+  { grille = plateau_vide; ships = [] }
+
+let emplacement_valide (p : plateau) (bateau : bateau) : bool = 
+  List.for_all (fun (x, y) ->
+    x >= 0 && x < Array.length p.grille &&  
+    y >= 0 && y < Array.length p.grille.(0) &&
+    match p.grille.(x).(y) with
+    | Vide -> true  
+    | _ -> false
+  ) bateau
+
+let generer_bateau (x : int) (y : int) (longueur : int) (horizontal : bool) : bateau =
+  List.init longueur (fun i -> if horizontal then (x, y+i) else (x+i, y))
+  
+let placer_bateau_valide (p : plateau) (x : int) (y : int) (longueur : int) (horizontal : bool) : bool =
+  let bateau = generer_bateau x y longueur horizontal in
+  if emplacement_valide p bateau then 
+(List.iter (fun (px,py) -> p.grille.(px).(py) <- Bateau (bateau)) bateau; 
+  p.ships <- bateau :: p.ships;
+  true) 
+else false
 end
